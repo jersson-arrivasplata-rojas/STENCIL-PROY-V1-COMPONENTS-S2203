fc30de45a6928e4af4e725302ad41d50874faff1
Jersson Arrivasplata Rojas <jerssonarrivasplata@bcp.com.pe>
2022-03-12 22:39:55 -0500

    Init Proyect

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'stencil-starter-project-name',
+  namespace: 'stencil-proy-v1-components-s2203',
   outputTargets: [
     {
       type: 'dist',
