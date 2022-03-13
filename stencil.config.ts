415556ec3703c3a209e7b851e2ff09960aa34401
Jersson Arrivasplata Rojas <jerssonarrivasplata@bcp.com.pe>
2022-03-13 12:47:29 -0500

    Proyect init

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,3 +1,4 @@
+import { angularOutputTarget } from '@stencil/angular-output-target';
 import { Config } from '@stencil/core';
 
 export const config: Config = {
@@ -16,6 +17,11 @@ export const config: Config = {
     {
       type: 'www',
       serviceWorker: null, // disable service workers
-    },
+    }/*,
+    angularOutputTarget({
+      componentCorePackage: 'sami',
+      directivesProxyFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/components.ts',
+      directivesArrayFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/index.ts',
+    })*/
   ],
 };
